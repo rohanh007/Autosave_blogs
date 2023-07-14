4d34efe6c969936c3c52c3351c7376f76fe123d9
rohanh007 <77897898+rohanh007@users.noreply.github.com>
2023-07-14 14:20:23 +0530

    Add files via upload

---
+++ client/src/pages/CreateBlog.js
@@ -0,0 +1,124 @@
+
+
+// export default CreateBlog;
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Box, Button, InputLabel, TextField, Typography } from "@mui/material";
+import toast from "react-hot-toast";
+
+const CreateBlog = () => {
+  const id = localStorage.getItem("userId");
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState({
+    title: "",
+    description: "",
+    image: "",
+  });
+
+  // input change
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInputs((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+
+    // Autosave
+    localStorage.setItem("autosavedBlogData", JSON.stringify(inputs));
+  };
+
+  // form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("/api/v1/blog/create-blog", {
+        title: inputs.title,
+        description: inputs.description,
+        image: inputs.image,
+        user: id,
+      });
+      if (data?.success) {
+        toast.success("Blog Created");
+        navigate("/my-blogs");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Load autosaved data on component mount
+  useEffect(() => {
+    const autosavedData = localStorage.getItem("autosavedBlogData");
+    if (autosavedData) {
+      setInputs(JSON.parse(autosavedData));
+    }
+  }, []);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <Box
+          width={"50%"}
+          border={3}
+          borderRadius={10}
+          padding={3}
+          margin="auto"
+          boxShadow={"10px 10px 20px #ccc"}
+          display="flex"
+          flexDirection={"column"}
+          marginTop="30px"
+        >
+          <Typography
+            variant="h2"
+            textAlign={"center"}
+            fontWeight="bold"
+            padding={3}
+            color="gray"
+          >
+            Create A Post
+          </Typography>
+          <InputLabel
+            sx={{ mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold" }}
+          >
+            Title
+          </InputLabel>
+          <TextField
+            name="title"
+            value={inputs.title}
+            onChange={handleChange}
+            margin="normal"
+            variant="outlined"
+            required
+          />
+          <InputLabel
+            sx={{ mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold" }}
+          >
+            Description
+          </InputLabel>
+          <TextField
+            name="description"
+            value={inputs.description}
+            onChange={handleChange}
+            margin="normal"
+            variant="outlined"
+            required
+          />
+          <InputLabel
+            sx={{ mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold" }}
+          >
+          
+          </InputLabel>
+          
+          <Button type="submit" color="primary" variant="contained">
+            SUBMIT
+          </Button>
+        </Box>
+      </form>
+    </>
+  );
+};
+
+export default CreateBlog;
+
